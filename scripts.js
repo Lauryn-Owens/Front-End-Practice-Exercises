3fec4ce3858dd86bdc32e4365632304a07f74997
Lauryn Owens <laurynowens83@gmail.com>
2024-07-18 18:48:27 -0400

    added comments to js scripts.js fixed button and winiing game functionality added rules text to html file- and styled it alittle

--- scripts.js
+++ scripts.js
@@ -1,69 +1,84 @@
+//allowable digits array
 const numberCharacters = [
     '0', '1', '2', '3', '4', '5' , '6' , '7', '8', 
     '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 
+//grab elements
 const guessColorBox = document.querySelector('.gameBody__color');
-const colorButtonsContainer = document.querySelector('.gameBody__guessColorContainer');
+const colorButtons = document.querySelectorAll('.gameBody__guessColorContainer__buttons--guessColor');
 const guessColor = generateGuessColor();
+//flag variable for showing game results text
 let showWinLoseText = false;
 const winLoseText = document.querySelector('.gameBody__outcome');
+const newGameButton  = document.querySelector('.ameBody__buttons--gameController');
 
+//create hex code colors
 function generateGuessColor(){
+    //initialize hex color
     let color = '#';
-    
+
+
+    //populate color string with elements in numberCharacters
     for(let i = 0; i < 6; i++){
         color += numberCharacters[Math.floor(Math.random() * numberCharacters.length)];
     }
+    //return populated finished color
     return color;
 }
-window.addEventListener('load', () => {
-       newGame();
-})
+
+//create a new game
 function newGame(){
+    //make box background color the generate global guess color
     guessColorBox.style.backgroundColor = guessColor;
+    //generate the button text content
       generateColorButtons();
+      //don't display game results if the user hasn't played yet
       if(!showWinLoseText){
         winLoseText.style.display = 'none';
       }
 
 }
-function colorButtonTemplate (textContent){
-    const colorButton = document.createElement('button');
-    colorButton.classList.add('gameBody__guessColorContainer__buttons--guessColor');
-    colorButton.textContent = textContent;
-    colorButton.addEventListener('click', (e) => {
-        console.log([e.target.textContent, guessColor])
-            playGame(e.target)
-    })
-    return colorButton;
-}
+
+//generate button text
 function generateColorButtons() {
-    const color =  guessColorBox.style.backgroundColor;
+    //create random number so each game the correct button is different/random placement
     const randomNumber = Math.floor(Math.random() * 3);
 
-    for(let i = 0; i < 3; i++){
-        if(i === randomNumber){ 
-             colorButtonsContainer.appendChild(
-               colorButtonTemplate(guessColor)
-             );
-        }
-        else{
-             colorButtonsContainer.appendChild(
-               colorButtonTemplate(generateGuessColor())
-             );
-        }
-    }
+    //loop through buttons and either give it the value the guessValue(to win the game) or a random hex color
+    colorButtons.forEach((currButton, currIdx) => {
+            if(currIdx === randomNumber){ 
+                currButton.textContent = guessColor;
+           }
+           else{
+              currButton.textContent = generateGuessColor();  
+           }
+        })
 }
 
+//display the win/lose game result text
 function playGame(button){
     winLoseText.style.display = 'block';
     if(button.textContent === guessColor){
-        winLoseText.textContent += 'Win!!!';
+        winLoseText.textContent = ' You Win!!!';
     }
     else{
-        winLoseText.textContent += 'Lose!!!';
+        winLoseText.textContent = 'You Lose!!!';
     }
-    setInterval(() =>{
-        winLoseText.style.display = 'none';
-    }, 1000);
+    colorButtons.forEach((currButton) => {
+        currButton.disabled = 'true';
+    })
 }
+//on window load create a new game
+window.addEventListener('load', () => {
+    newGame();
+})
+//for each button run the playGame function to see if the play guessed correctly
+colorButtons.forEach((currButton) => {
+    currButton.addEventListener('click', (e) => {
+            playGame(e.target)
+    })
+})
+//reload the tab to restart and create a new a new game
+document.querySelector('.gameBody__buttons--gameController').addEventListener('click',() =>{
+    window.location.reload();
+})  
