5d03880634538c457355580a31569271112b78d4
Lauryn Owens <laurynowens83@gmail.com>
2024-08-28 20:22:28 -0400

    added commits in the script.js file

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ let resultsShown = false;
 let showEmptyFieldsMessage = false;
 
 function grabElements(){
-    //inputs
+    // inputs
     const mortgageAmount = document.querySelector('#calculator__amount');
     const mortgageTerm = document.querySelector('#term');
     const mortgageInterest = document.querySelector('#interest');
@@ -12,18 +12,17 @@ function grabElements(){
     const clearAll = document.querySelector('.calculator__clear');
     const allInputs = document.getElementsByTagName('input');
 
-    //number text
+    // number text
     const repaymentMonthly = document.querySelector('#repayment_monthly');
     const repaymentTotal = document.querySelector('#repayment_total');
 
-    //calculator form not submitted
+    // calculator form not submitted
     const calculatorUnsubmitted = document.querySelector('.calculator__right-unsubmitted');
    
-    //calculator right repayment calculations
+    // calculator right repayment calculations
     const calculatorCalculation = document.querySelector('.calculate__right-hidden');
    
-   //elements that change state if input fields are empty on submit
-    //.calculator__error, .empty
+    // elements that change state if input fields are empty on submit
     const showEmptyFieldsMessage = document.querySelectorAll('.calculator__error');
     const emptyInputSpan = document.querySelectorAll('.empty');
     
@@ -43,6 +42,7 @@ function grabElements(){
         emptyInputSpan:emptyInputSpan
     };
 }
+
 function displayEmptyMessage(){
     grabElements().showEmptyFieldsMessage.forEach((currMessage) => {
         const computedStyle = window.getComputedStyle(currMessage);
@@ -51,120 +51,144 @@ function displayEmptyMessage(){
             currMessage.style.display = 'none';
             return;
         }
-       else{
-        if(computedStyle.display === 'none'){
-            currMessage.style.display = 'block';
-            return;
+        else{
+            if(computedStyle.display === 'none'){
+                currMessage.style.display = 'block';
+                return;
+            }
         }
-       }
-     })
+    })
 }
+
 function calculateRepaymentMortgage(loanAmount, annualInterestRate, loanTermYears) {
+    // calculate monthly interest rate
     let monthlyInterestRate = annualInterestRate / 100 / 12;
+
+    // calculate total number of payments
     let numberOfPayments = loanTermYears * 12;
 
+    // calculate monthly payment
     let monthlyPayment = (loanAmount * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) /
                          (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
 
     return monthlyPayment;
 }
+
 function calculateInterestOnlyMortgage(loanAmount, annualInterestRate) {
+    // calculate monthly interest rate
     let monthlyInterestRate = annualInterestRate / 100 / 12;
 
+    // calculate monthly payment for interest only mortgage
     let monthlyPayment = loanAmount * monthlyInterestRate;
 
     return monthlyPayment;
 }
+
 function getType(){
+    // set the selected type based on user selection
     selectedType = this.value;
 }
+
 function calculateRepayment(e){
+    // prevent the default form submission
     e.preventDefault();
+
     let months = grabElements().mortgageTerm.value * 12;
-   if(grabElements().mortgageAmount.value &&
-    grabElements().mortgageTerm.value &&
-    grabElements().mortgageInterest.value &&
-    (selectedType === 'repayment') 
-){
-    let repaymentMonthlyPayment = calculateRepaymentMortgage(parseFloat(grabElements().mortgageAmount.value), 
-    parseFloat(grabElements().mortgageInterest.value), parseFloat(grabElements().mortgageTerm.value));
-    grabElements().repaymentMonthly.textContent = repaymentMonthlyPayment.toFixed(2);
-    grabElements().repaymentTotal.textContent = (repaymentMonthlyPayment * months).toFixed(2);
-    
-    grabElements().calculatorUnsubmitted.style.display = 'none';
-    grabElements().calculatorCalculation.style.display = 'block';
 
-    displayEmptyMessage();
-    /*
-    grabElements().showEmptyFieldsMessage.forEach((currMessage) => {
-        const computedStyle = window.getComputedStyle(currMessage);
+    // check if all input fields are filled and the repayment type is selected
+    if(grabElements().mortgageAmount.value &&
+       grabElements().mortgageTerm.value &&
+       grabElements().mortgageInterest.value &&
+       (selectedType === 'repayment')) {
+        
+        // calculate repayment mortgage monthly payment
+        let repaymentMonthlyPayment = calculateRepaymentMortgage(parseFloat(grabElements().mortgageAmount.value), 
+        parseFloat(grabElements().mortgageInterest.value), parseFloat(grabElements().mortgageTerm.value));
 
-        if(computedStyle.display === 'block'){
-            currMessage.style.display = 'none';
-        }
-     })
-    */
-     grabElements().emptyInputSpan.forEach((currInputSpan) => {
-        currInputSpan.style.backgroundColor = 'lightblue';
-        currInputSpan.style.color = 'black';
-     })
-    
-   }
-   else if(grabElements().mortgageAmount.value &&
-   grabElements().mortgageTerm.value &&
-   grabElements().mortgageInterest.value &&
-   (selectedType === 'interest_only') ){
-    let interestOnlyMonthlyPayment = calculateInterestOnlyMortgage(parseFloat(grabElements().mortgageAmount.value), 
-    parseFloat(grabElements().mortgageInterest.value));
-    grabElements().repaymentMonthly.textContent = interestOnlyMonthlyPayment.toFixed(2);
-    grabElements().repaymentTotal.textContent = (interestOnlyMonthlyPayment * months).toFixed(2);
+        // update the monthly and total repayment fields
+        grabElements().repaymentMonthly.textContent = repaymentMonthlyPayment.toFixed(2);
+        grabElements().repaymentTotal.textContent = (repaymentMonthlyPayment * months).toFixed(2);
+        
+        // hide the unsubmitted form and show the calculated results
+        grabElements().calculatorUnsubmitted.style.display = 'none';
+        grabElements().calculatorCalculation.style.display = 'block';
 
-    grabElements().calculatorUnsubmitted.style.display = 'none';
-    grabElements().calculatorCalculation.style.display = 'block';
+        // toggle display of empty fields message
+        displayEmptyMessage();
+        
+        // update the appearance of empty input spans
+        grabElements().emptyInputSpan.forEach((currInputSpan) => {
+            currInputSpan.style.backgroundColor = 'lightblue';
+            currInputSpan.style.color = 'black';
+        });
+    }
+    else if(grabElements().mortgageAmount.value &&
+            grabElements().mortgageTerm.value &&
+            grabElements().mortgageInterest.value &&
+            (selectedType === 'interest_only')) {
+        
+        // calculate interest only mortgage monthly payment
+        let interestOnlyMonthlyPayment = calculateInterestOnlyMortgage(parseFloat(grabElements().mortgageAmount.value), 
+        parseFloat(grabElements().mortgageInterest.value));
 
-    grabElements().showEmptyFieldsMessage.forEach((currMessage) => {
-        const computedStyle = window.getComputedStyle(currMessage);
+        // update the monthly and total repayment fields
+        grabElements().repaymentMonthly.textContent = interestOnlyMonthlyPayment.toFixed(2);
+        grabElements().repaymentTotal.textContent = (interestOnlyMonthlyPayment * months).toFixed(2);
 
-        if(computedStyle.display === 'block'){
-            currMessage.style.display = 'none';
-        }
-     })
+        // hide the unsubmitted form and show the calculated results
+        grabElements().calculatorUnsubmitted.style.display = 'none';
+        grabElements().calculatorCalculation.style.display = 'block';
 
-     grabElements().emptyInputSpan.forEach((currInputSpan) => {
-        currInputSpan.style.backgroundColor = 'lightblue';
-        currInputSpan.style.color = 'black';
-     })
-   }
-   else{
-         grabElements().showEmptyFieldsMessage.forEach((currMessage) => {
+        // toggle display of empty fields message
+        grabElements().showEmptyFieldsMessage.forEach((currMessage) => {
+            const computedStyle = window.getComputedStyle(currMessage);
+
+            if(computedStyle.display === 'block'){
+                currMessage.style.display = 'none';
+            }
+        });
+
+        // update the appearance of empty input spans
+        grabElements().emptyInputSpan.forEach((currInputSpan) => {
+            currInputSpan.style.backgroundColor = 'lightblue';
+            currInputSpan.style.color = 'black';
+        });
+    }
+    else{
+        // show empty fields message and highlight the empty fields
+        grabElements().showEmptyFieldsMessage.forEach((currMessage) => {
             const computedStyle = window.getComputedStyle(currMessage);
 
             if(computedStyle.display === 'none'){
                 currMessage.style.display = 'block';
             }
-         })
-         grabElements().emptyInputSpan.forEach((currInputSpan) => {
+        });
+        grabElements().emptyInputSpan.forEach((currInputSpan) => {
             currInputSpan.style.backgroundColor = 'red';
             currInputSpan.style.color = 'white';
-         })
-   }
+        });
+    }
 }
+
 function clearAll(){
+    // clear all input fields
     Array.from(grabElements().allInputs).forEach((curr) => {
         if(curr.type === 'radio'){
-                curr.checked = false;
+            curr.checked = false;
         }
         else{
             curr.value = '';
         }
+        // reset the form display to the unsubmitted state
         grabElements().calculatorUnsubmitted.style.display = 'block';
         grabElements().calculatorCalculation.style.display = 'none';
-    })
+    });
 }
 
+// initialize elements and event listeners on window load
 window.onload = grabElements();
 grabElements().calculateButton.addEventListener('click', calculateRepayment);
 grabElements().repaymentType.forEach((currType) => {
     currType.addEventListener('change', getType);
-})
-grabElements().clearAll.addEventListener('click', clearAll)
+});
+grabElements().clearAll.addEventListener('click', clearAll);
